360e03a7ce0cd0db992aa7667aaeaa4495634e3e
Kim Lewis <kalewi2@gmail.com>
2019-08-31 21:41:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,5 @@
 def find_element_index(array, value_to_find)
-  i = 0
-  while i <= array.length do
-    puts value_to_find
-    i += 1
-  end
-  return array.index(value_to_find)
+    return array.index(value_to_find)
 end
 
 def find_max_value(array)
