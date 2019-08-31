644f117958cf778bf826477fe6467df3346a498a
Kim Lewis <kalewi2@gmail.com>
2019-08-31 21:34:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,9 +8,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  i = 0 
-  while i <= array.length do
-    array[i] 
+  return array.max 
 end
 
 def find_min_value(array)
